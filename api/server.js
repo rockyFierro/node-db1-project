7f46a00c1c2ffe9e167615aeeae8273e2e8ea617
Rocky Fierro <39335873+rockyFierro@users.noreply.github.com>
2021-07-21 20:17:04 -0700

    server wired to router

--- api/server.js
+++ api/server.js
@@ -1,7 +1,14 @@
 const express = require("express");
-
+const accountsRouter = require('./accounts/accounts-router');
 const server = express();
 
+server.use('/api/accounts', accountsRouter);
+
+server.get('/', (req,res)=>{
+  res.status(200).json({
+    api: 'up',
+  });
+});
 server.use(express.json());
 
 module.exports = server;
